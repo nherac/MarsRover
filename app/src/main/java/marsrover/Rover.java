323fee0cf5373ba2b704bb2f83a7f18a23f95e75
nherac <carmen@gamezlara.com>
2021-05-29 01:40:09 +0100

    Single rover moves following commands in unlimited land.

--- app/src/main/java/marsrover/Rover.java
+++ app/src/main/java/marsrover/Rover.java
@@ -13,6 +13,16 @@ public class Rover {
 
     public void setAngle(int angle) {
         angle = angle % 360;
+        boolean negativeAngle = (angle<0);
+        if(negativeAngle){
+            var positiveAngle = switch(angle){
+                case -90 ->270;
+                case -180 ->180;
+                case -270 ->90;
+                default -> throw new IllegalArgumentException("This angle is not supported");
+            };
+            angle = positiveAngle;
+        }
         this.angle = angle;
     }
 
